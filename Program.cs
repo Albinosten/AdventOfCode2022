ddbefb1ec43e64bafdcf8e95d479ee6008a639b2
Albin <abbeohebbe@live.se>
2022-12-07 12:03:32 +0100

    day5 driver code

--- Program.cs
+++ Program.cs
@@ -10,11 +10,17 @@ namespace AdventOfCode2022
             OutputResult(new Puzzle2());
             OutputResult(new Puzzle3());
             OutputResult(new Puzzle4());
-
+            OutputResult(new Puzzle5());
             OutputResult(new Puzzle6());
         }
 
         private static void OutputResult(IPuzzle puzzle)
+        {
+            Console.WriteLine("Now solving: " + puzzle.GetType().Name);
+            Console.WriteLine("First: " + puzzle.Solve() + " Should be: " + puzzle.FirstResult);
+            Console.WriteLine("Second: " + puzzle.SolveNext() + " Should be: " + puzzle.SecondResult);
+        }
+        private static void OutputResult(IPuzzle<string> puzzle)
         {
             Console.WriteLine("Now solving: " + puzzle.GetType().Name);
             Console.WriteLine("First: " + puzzle.Solve() + " Should be: " + puzzle.FirstResult);
